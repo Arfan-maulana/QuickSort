97411b95e0c85121552d328223ee7512faecd2e0
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-10-17 16:54:07 +0700

    Add project files.

---
+++ QuickSort/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        //array of interger to hold values
+        private int[] arr = new int[20];
+        private int cmp_count = 0; // number of comprasion
+        private int mov_count = 0; // number of data movements
+
+
+
+           }
+}
