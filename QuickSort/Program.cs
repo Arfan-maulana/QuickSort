b6fd6ae35cecd2c14f347f843bfa98b38947a50b
Muhammad Arfan <muhammad.arfan.ft21@mail.umy.ac.id>
2022-10-17 17:03:23 +0700

    number of element in array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -13,6 +13,31 @@ namespace QuickSort
         private int cmp_count = 0; // number of comprasion
         private int mov_count = 0; // number of data movements
 
+        // Nimber od elements in array 
+        private int n;
+
+        void read()
+        {
+            while(true)
+            {
+                Console.Write("Enter The number of elemnts in the array :");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 30 elements \n ");
+            }
+            Console.WriteLine("\n=====================");
+            Console.WriteLine(" Enter Array Elements");
+            Console.WriteLine("\n=====================");
+
+
+
+
+        }
+            
+           
 
 
            }
